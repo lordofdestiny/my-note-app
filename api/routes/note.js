e1aa2c6e6afbc77e10885dde1b7784611304f63d
Djumic Branislav <djumicbranislav@outlook.com>
2019-12-04 18:52:48 +0100

    |Update #1| Minor changes

--- api/routes/note.js
+++ api/routes/note.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const SSE = require("express-sse");
 const mongoose = require("mongoose");
-const moment = require("moment");
 const router = express.Router();
 
 const authenticate = require("../middleware/auth");
@@ -11,7 +10,8 @@ const User = require("../models/user");
 
 const sse = new SSE([], { isSerialized: false, initialEvent: "all" });
 
-router.get("/", authenticate(), (req, res, next) => {
+//get all notes as array
+router.get("/", authenticate(), (req, res) => {
   const { _id } = req.user;
   Note.find({ user_id: _id })
     .exec()
@@ -25,21 +25,19 @@ router.get("/", authenticate(), (req, res, next) => {
 
 router.get("/sse", sse.init);
 
-router.post("/", authenticate(), (req, res, next) => {
+router.post("/", authenticate(), (req, res) => {
   const { _id } = req.user;
   const user_id = new mongoose.Types.ObjectId(_id);
   const { flash_date } = req.body;
-
-  console.log(flash_date);
 
   const note = new Note({ user_id, ...req.body, flash_date });
   note
     .save()
     .then(note => {
       res.status(200).json({ note });
-      sse.send(note, "new");
+      //sse.send(note, "new");
       const { _id, user_id } = note;
-      User.updateOne({ _id: user_id }, { $push: { notes: _id } }).exec();
+      return User.updateOne({ _id: user_id }, { $push: { notes: _id } }).exec();
     })
     .catch(error => {
       res.status(500).json({ ...error });

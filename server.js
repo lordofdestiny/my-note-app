a5a5c168aefcf61f19a05df9db52b067f409dd7f
lordofdestiny <42172334+lordofdestiny@users.noreply.github.com>
2022-07-07 23:21:05 +0200

    Increased code quality

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const http = require("http");
-const config = require("./utlis/config");
-const app = require("./app.js");
+const { getEnv, getArgs } = require("./utlis/config");
 
-const port = config.port || 3000;
+const { port: envPort } = getEnv(true);
+const { port: argPort } = getArgs({ port: null });
+const port = argPort ?? envPort;
+
+const app = require("./app.js");
 
 const server = http.createServer(app);
 
 server.listen(port, () => console.log(`Server listening on port ${port}`));
+process.addListener("beforeExit", () => require("mongoose").connection.close());

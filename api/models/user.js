5108cae8ce8ba4b6751751ebe57b5bd3aabfc0a8
lordofdestiny <42172334+lordofdestiny@users.noreply.github.com>
2022-07-07 23:27:26 +0200

    replaced static functions with custom validators

--- api/models/user.js
+++ api/models/user.js
@@ -1,19 +1,43 @@
 const mongoose = require("mongoose");
 const { toTitleCase } = require("../../utlis/helpers");
 
+const uniqueValueValidator = (modelName, filedName, message) => {
+  return async (value) => {
+    return new Promise(async (resolve, reject) => {
+      const user = await mongoose
+        .model(modelName)
+        .find({ [filedName]: value })
+        .exec();
+      if (user.length >= 1) {
+        reject(message);
+      } else {
+        resolve(true);
+      }
+    });
+  };
+};
+
 const userSchema = mongoose.Schema({
   _id: {
     type: mongoose.Schema.Types.ObjectId,
-    required: [true, "Empty _id is not allowed!"]
+    required: [true, "Empty _id is not allowed!"],
   },
-  first_name: { type: String, required: false },
-  last_name: { type: String, required: false },
-  phone: { type: String, required: false },
+  first_name: { type: String, required: false, default: () => null },
+  last_name: { type: String, required: false, default: () => null },
+  phone: { type: String, required: false, default: () => null },
   email: {
     type: String,
     required: [true, "Empty email is not allowed"],
     unique: true,
-    match: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    match:
+      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+    validate: {
+      validator: uniqueValueValidator(
+        "User",
+        "email",
+        "This email address is already taken!"
+      ),
+    },
   },
   username: {
     type: String,
@@ -21,53 +45,32 @@ const userSchema = mongoose.Schema({
     unique: true,
     match: /^[a-zA-Z0-9]/,
     minlength: 8,
-    maxlength: 25
+    maxlength: 25,
+    validate: {
+      validator: uniqueValueValidator(
+        "User",
+        "username",
+        "This username is already in use!"
+      ),
+    },
   },
   password: {
     type: String,
     required: [true, "Empty password is not allowed!"],
-    match: /^[a-zA-Z0-9!@#$%^&* !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/
+    match: /^[a-zA-Z0-9!@#$%^&* !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/,
   },
   gender: {
     type: String,
-    require: true,
-    enum: ["male", "female", "rns"]
+    require: false,
+    enum: ["male", "female", "rns", "unset"],
+    default: () => "unset",
   },
   notes: [
     {
       type: mongoose.Schema.Types.ObjectId,
-      ref: "Note"
-    }
-  ]
+      ref: "Note",
+    },
+  ],
 });
-
-//Static method that's used to check do
-//email and username exist before registring the user
-userSchema.statics.checkField = function(field, value) {
-  return new Promise((resolve, reject) => {
-    if (!value) {
-      const error = new Error(`${toTitleCase(field)} not provided!`);
-      error.status = 400;
-      reject(error);
-    } else {
-      this.find({ [field]: value })
-        .exec()
-        .then(user => {
-          if (user.length >= 1) {
-            const error = new Error(
-              `A user with this ${field} already exists!`
-            );
-            error.status = 409;
-            reject(error);
-          } else {
-            resolve();
-          }
-        })
-        .catch(error => {
-          reject(error);
-        });
-    }
-  });
-};
 
 module.exports = mongoose.model("User", userSchema);

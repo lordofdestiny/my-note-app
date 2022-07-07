73f65652c70a3ade230d25839155553ac4eeeee9
lordofdestiny <42172334+lordofdestiny@users.noreply.github.com>
2022-07-07 23:29:35 +0200

    No significant change

--- api/routes/user.js
+++ api/routes/user.js
@@ -9,22 +9,27 @@ router.post("/signup", UserControllers.user_singup);
 
 router.post(
   "/login",
+  (req, res, next) => {
+    req.flash("username", req.body.username);
+    next();
+  },
   passport.authenticate("local", {
     successRedirect: "/",
     failureRedirect: "/login",
-    failureFlash: true
+    failureFlash: true,
   })
 );
 
 router.get("/logout", UserControllers.user_logout);
 
 passport.serializeUser((user, done) => {
+  console.log(user);
   done(null, user._id);
 });
 
 passport.deserializeUser((_id, done) => {
   User.findById({ _id }, (error, user) => {
-    done(error, user);
+    done(error, user.toObject());
   });
 });
 

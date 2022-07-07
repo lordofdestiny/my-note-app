48f84913b3401771e2a67080828269b0cdafd702
lordofdestiny <42172334+lordofdestiny@users.noreply.github.com>
2022-07-07 23:31:37 +0200

    More soffisticated config manager file

--- utlis/config.js
+++ utlis/config.js
@@ -1,6 +1,51 @@
-const { COOKIE_SECRET, PORT } = process.env;
+function fallbackDefault(val, defVal = "", conversion) {
+  const normalized = val ?? defVal;
+  const convert = conversion ?? String;
+  return convert(normalized);
+}
+
+const storage = {};
+let initial = true;
+
+const getEnv = (refresh = false) => {
+  if (initial || refresh) {
+    const {
+      PORT,
+      SESSION_SECRET,
+      DB_USERNAME,
+      DB_PASSWORD,
+      DB_HOST,
+      DB_PORT,
+      DB_NAME,
+    } = process.env;
+    Object.assign(storage, {
+      port: fallbackDefault(PORT, null, Number),
+      secret: fallbackDefault(SESSION_SECRET),
+      database: {
+        username: fallbackDefault(DB_USERNAME),
+        password: fallbackDefault(DB_PASSWORD),
+        host: fallbackDefault(DB_HOST, "localhost"),
+        port: fallbackDefault(DB_PORT, 27017, Number),
+        name: fallbackDefault(DB_NAME, "note-app"),
+      },
+    });
+    initial = false;
+  }
+
+  return storage;
+};
+
+const getArgs = (defaults = {}) => {
+  const args = process.argv.slice(2);
+  const parser = /^-{0,2}(?<name>[a-z0-9]+)=(?<value>[a-z0-9]+)$/;
+  const parsed = args.reduce((acc, arg) => {
+    const { name, value } = parser.exec(arg).groups;
+    return Object.assign(acc, { [name]: Number(value) || value });
+  }, {});
+  return Object.assign(defaults, parsed);
+};
 
 module.exports = {
-  secret: COOKIE_SECRET,
-  port: PORT
+  getEnv,
+  getArgs,
 };

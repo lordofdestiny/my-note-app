771244fbcd9e2f040b006217f2e7ba3221056551
lordofdestiny <42172334+lordofdestiny@users.noreply.github.com>
2022-07-07 23:23:27 +0200

    upgraded authentication

--- api/middleware/auth.js
+++ api/middleware/auth.js
@@ -1,10 +1,25 @@
-module.exports = () => {
+const authenticate = () => {
   return (req, res, next) => {
     if (req.isAuthenticated()) {
-      console.log(`User ID: ${JSON.stringify(req.session.passport.user)}`);
+      //console.log(`User ID: ${JSON.stringify(req.session.passport.user)}`);
       return next();
     }
 
     res.redirect("/login");
   };
+};
+
+const skipIfAuthenticated = () => {
+  return (req, res, next) => {
+    if (req.isAuthenticated()) {
+      res.redirect("/");
+    } else {
+      next();
+    }
+  };
+};
+
+module.exports = {
+  authenticate,
+  skipIfAuthenticated,
 };

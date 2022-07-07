d07d067d916bf4d65f7c57550bedbada2bf95af8
lordofdestiny <42172334+lordofdestiny@users.noreply.github.com>
2022-07-07 23:26:30 +0200

    Added commas after every line

--- api/models/note.js
+++ api/models/note.js
@@ -4,29 +4,29 @@ const noteSchema = mongoose.Schema({
   user_id: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "User",
-    required: [true, "Note must have a user!"]
+    required: [true, "Note must have a user!"],
   },
   date_created: {
     type: Date,
-    default: () => new Date()
+    default: () => new Date(),
   },
   date_modified: {
     type: Date,
-    default: () => new Date()
+    default: () => new Date(),
   },
   title: {
     type: String,
-    required: [true, "Note must have a title!"]
+    required: [true, "Note must have a title!"],
   },
   text: {
     type: String,
-    required: [true, "Note must have a text!"]
+    required: [true, "Note must have a text!"],
   },
   flash_date: {
     type: Date,
-    default: null
+    default: null,
   },
-  media: []
+  media: [],
 });
 
 module.exports = mongoose.model("Note", noteSchema);
